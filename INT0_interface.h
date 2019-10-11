e65c337b995be80ced469c15098db63aaa5632da
Khaledgowied <56441183+Khaledgowied@users.noreply.github.com>
2019-10-11 17:16:37 +0200

    Add files via upload

---
+++ INT0_interface.h
@@ -0,0 +1,13 @@
+/*
+ * INT0_interface.h
+ *
+ *  Created on: Jul 10, 2018
+ *      Author: Sedra Frimware
+ */
+
+#ifndef INT0_INTERFACE_H_
+#define INT0_INTERFACE_H_
+
+void INT0_Inits(void);
+
+#endif /* INT0_INTERFACE_H_ */
---
+++ INT1_interface.h
@@ -0,0 +1,13 @@
+/*
+ * INT1_interface.h
+ *
+ *  Created on: Jul 10, 2018
+ *      Author: Sedra Frimware
+ */
+
+#ifndef INT1_INTERFACE_H_
+#define INT1_INTERFACE_H_
+
+void INT1_Inits(void);
+
+#endif /* INT1_INTERFACE_H_ */
